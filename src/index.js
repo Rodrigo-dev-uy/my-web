22181007d98fd4ac26b097d3ac1142940cde9754
Rodrigo Costa <ro.costa.dev@gmail.com>
2023-06-05 17:34:30 -0300

    my-web

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import Footer from "./components/Footer.jsx";
 import Loader from "./components/Loader";
 
 const Router = createBrowserRouter([
-  { path: "/my-web", element: <App /> },
+  { path: "/", element: <App /> },
   { path: "/loader", element: <Loader /> },
   { path: "/home", element: <Home /> },
   { path: "/about", element: <About /> },
