39db6668ed20afc1738f816eef1ca49a5651d79e
Rodrigo Costa <ro.costa.dev@gmail.com>
2023-06-05 17:10:10 -0300

    rest

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,47 @@
-import yoImage from "../assets/tech/yo3.png";
+import React from "react";
+import { Link } from "react-scroll";
+import { Parallax } from "react-parallax";
+import FigmaImage from "../assets/tech/figma.png";
 
 function About() {
   return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas inventore
-        assumenda blanditiis beatae est et repudiandae a. Maiores ex,
-        repudiandae aliquid quidem odio, magnam sint, dolore ipsa exercitationem
-        quis error?
-      </p>
-      <img src={yoImage} alt="Yo" />
-      About
+    <div id="about-section">
+      <Parallax bgImage={FigmaImage} strength={500}>
+        <div className="parallax-content">
+          <h1>Título de la sección</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur
+            eaque rem mollitia, quis necessitatibus accusantium natus eveniet
+            deleniti laboriosam obcaecati cum blanditiis nisi quo repellendus
+            ipsa ut cupiditate architecto sit? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Aspernatur deserunt eaque magnam
+            laborum itaque corporis commodi harum ea vel repudiandae voluptates,
+            dolore minima praesentium rerum molestiae ducimus illo voluptas
+            nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Ratione, cumque. Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Facilis possimus nam fuga. Dolor, fugiat nemo blanditiis odio
+            ab quasi, deserunt dolorum suscipit incidunt corrupti adipisci totam
+            nam, vel omnis libero! Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Est id iusto quibusdam nemo eius mollitia itaque
+            distinctio ea laudantium quos? Sed a enim et cumque ipsam esse
+            repellat earum architecto? Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Ad, delectus quas quod eligendi molestias eveniet
+            saepe a ut quo cupiditate voluptatum excepturi, placeat accusantium
+            cum commodi? Minus blanditiis tempore natus?
+          </p>
+        </div>
+      </Parallax>
+
+      <Link
+        activeClass="active"
+        to="home-section"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+      >
+        Back to Home
+      </Link>
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,33 +1,188 @@
-import { useNavigate } from "react-router";
+import { gsap } from "gsap";
+import React, { useEffect } from "react";
 import "../styles/Home.css";
+import Logo from "../assets/tech/3.png";
+import Iam from "../assets/tech/yo1.png";
+import { useNavigate } from "react-router-dom";
+import { Parallax } from "react-parallax";
+import { Link } from "react-scroll";
+
 function Home() {
   const navigate = useNavigate();
 
   return (
-    <div className="hero">
-      <nav>
-        <h2 className="logo">
-          Rodrigo <span className="name">Costa</span>{" "}
-        </h2>
-        <ul>
-          <li>
-            <span className="click" onClick={() => navigate("/about")}>
-              About Me
-            </span>
-          </li>
-          <li>
-            <span className="click" onClick={() => navigate("/estudios")}>
-              Estudios
-            </span>
-          </li>
-          <li>
-            <span className="click" onClick={() => navigate("/contact")}>
-              Contact
-            </span>
-          </li>
-        </ul>
-        <button>Github</button>
-      </nav>
+    <div className="container">
+      <img className="logo" src={Logo} alt="Yo" />
+
+      <div class="hello-parent">
+        <svg class="hello-word" width="365" height="365" viewBox="0 0 365 365">
+          <g id="H-letter">
+            <line
+              class="H-left-stroke"
+              x1="17"
+              y1="0"
+              x2="17"
+              y2="124"
+              stroke="#CC2900"
+              fill="none"
+              stroke-width="34"
+            />
+            <line
+              class="H-mid-stroke"
+              x1="33"
+              y1="62"
+              x2="68"
+              y2="62"
+              stroke="#000"
+              fill="none"
+              stroke-width="34"
+            />
+            <line
+              class="H-right-stroke"
+              x1="84"
+              y1="0"
+              x2="84"
+              y2="124"
+              stroke="#000"
+              fill="none"
+              stroke-width="34"
+            />
+          </g>
+
+          <g id="E-letter">
+            <line
+              class="E-left-stroke"
+              x1="138"
+              y1="0"
+              x2="138"
+              y2="124"
+              stroke="#000"
+              fill="none"
+              stroke-width="34"
+            />
+            <line
+              class="E-top-stroke"
+              x1="154"
+              y1="17"
+              x2="201"
+              y2="17"
+              stroke="#000"
+              fill="none"
+              stroke-width="34"
+            />
+            <line
+              class="E-mid-stroke"
+              x1="154"
+              y1="62"
+              x2="196"
+              y2="62"
+              stroke="#000"
+              fill="none"
+              stroke-width="34"
+            />
+            <line
+              class="E-bottom-stroke"
+              x1="154"
+              y1="107"
+              x2="201"
+              y2="107"
+              stroke="#000"
+              fill="none"
+              stroke-width="34"
+            />
+          </g>
+
+          <g id="L-one-letter">
+            <line
+              class="L-one-long-stroke"
+              x1="17"
+              y1="153"
+              x2="17"
+              y2="277"
+              stroke="#000"
+              fill="none"
+              stroke-width="34"
+            />
+            <line
+              class="L-one-short-stroke"
+              x1="33"
+              y1="260"
+              x2="77"
+              y2="260"
+              stroke="#000"
+              fill="none"
+              stroke-width="34"
+            />
+          </g>
+
+          <g id="L-two-letter">
+            <line
+              class="L-two-long-stroke"
+              x1="104"
+              y1="153"
+              x2="104"
+              y2="277"
+              stroke="#000"
+              fill="none"
+              stroke-width="34"
+            />
+            <line
+              class="L-two-short-stroke"
+              x1="120"
+              y1="260"
+              x2="164"
+              y2="260"
+              stroke="#000"
+              fill="none"
+              stroke-width="34"
+            />
+          </g>
+
+          <g id="O-letter">
+            <circle
+              class="O-stroke"
+              cx="231"
+              cy="215"
+              r="48"
+              stroke="#000"
+              fill="none"
+              stroke-width="31"
+            />
+          </g>
+
+          <g id="red-dot">
+            <circle
+              class="red-dot"
+              cx="325"
+              cy="260"
+              r="20"
+              fill="#e62e00"
+              stroke="none"
+            />
+
+            <line
+              x1="325"
+              y1="260"
+              x2="325"
+              y2="260"
+              stroke="#cc2900"
+              class="red-dot"
+            />
+          </g>
+        </svg>
+      </div>
+      <img className="iam" src={Iam} alt="Yo" />
+      <h2 className="name2">WORLD</h2>
+      <Link
+        activeClass="active"
+        to="about-section"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+      >
+        Scroll to About
+      </Link>
     </div>
   );
 }
