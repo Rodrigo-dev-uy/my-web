314f8158d84ede46de98e854a44443c4160583bd
Rodrigo Costa <ro.costa.dev@gmail.com>
2023-06-07 01:04:02 -0300

    home

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,126 @@
 import React from "react";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum
+      temporibus eos, iusto doloremque deleniti eius exercitationem, veritatis
+      ipsum tempora doloribus consequatur eveniet magnam, error vel. Incidunt
+      harum odio et officiis!Loremlorem Lorem ipsum dolor sit amet consectetur,
+      adipisicing elit. Voluptatum temporibus eos, iusto doloremque deleniti
+      eius exercitationem, veritatis ipsum tempora doloribus consequatur eveniet
+      magnam, error vel. Incidunt harum odio et officiis!Loremlorem Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptatum temporibus eos,
+      iusto doloremque deleniti eius exercitationem, veritatis ipsum tempora
+      doloribus consequatur eveniet magnam, error vel. Incidunt harum odio et
+      officiis!Loremlorem Lorem ipsum dolor sit amet consectetur, adipisicing
+      elit. Voluptatum temporibus eos, iusto doloremque deleniti eius
+      exercitationem, veritatis ipsum tempora doloribus consequatur eveniet
+      magnam, error vel. Incidunt harum odio et officiis!Loremlorem Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptatum temporibus eos,
+      iusto doloremque deleniti eius exercitationem, veritatis ipsum tempora
+      doloribus consequatur eveniet magnam, error vel. Incidunt harum odio et
+      officiis!Loremlorem Lorem ipsum dolor sit amet consectetur, adipisicing
+      elit. Voluptatum temporibus eos, iusto doloremque deleniti eius
+      exercitationem, veritatis ipsum tempora doloribus consequatur eveniet
+      magnam, error vel. Incidunt harum odio et officiis!Loremlorem Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptatum temporibus eos,
+      iusto doloremque deleniti eius exercitationem, veritatis ipsum tempora
+      doloribus consequatur eveniet magnam, error vel. Incidunt harum odio et
+      officiis!Loremlorem Lorem ipsum dolor sit amet consectetur, adipisicing
+      elit. Voluptatum temporibus eos, iusto doloremque deleniti eius
+      exercitationem, veritatis ipsum tempora doloribus consequatur eveniet
+      magnam, error vel. Incidunt harum odio et officiis!Loremlorem Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptatum temporibus eos,
+      iusto doloremque deleniti eius exercitationem, veritatis ipsum tempora
+      doloribus consequatur eveniet magnam, error vel. Incidunt harum odio et
+      officiis!Loremlorem Lorem ipsum dolor sit amet consectetur, adipisicing
+      elit. Voluptatum temporibus eos, iusto doloremque deleniti eius
+      exercitationem, veritatis ipsum tempora doloribus consequatur eveniet
+      magnam, error vel. Incidunt harum odio et officiis!Loremlorem Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptatum temporibus eos,
+      iusto doloremque deleniti eius exercitationem, veritatis ipsum tempora
+      doloribus consequatur eveniet magnam, error vel. Incidunt harum odio et
+      officiis!Loremlorem Lorem ipsum dolor sit amet consectetur, adipisicing
+      elit. Voluptatum temporibus eos, iusto doloremque deleniti eius
+      exercitationem, veritatis ipsum tempora doloribus consequatur eveniet
+      magnam, error vel. Incidunt harum odio et officiis!Loremlorem Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptatum temporibus eos,
+      iusto doloremque deleniti eius exercitationem, veritatis ipsum tempora
+      doloribus consequatur eveniet magnam, error vel. Incidunt harum odio et
+      officiis!Loremlorem Lorem ipsum dolor sit amet consectetur, adipisicing
+      elit. Voluptatum temporibus eos, iusto doloremque deleniti eius
+      exercitationem, veritatis ipsum tempora doloribus consequatur eveniet
+      magnam, error vel. Incidunt harum odio et officiis!Loremlorem Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptatum temporibus eos,
+      iusto doloremque deleniti eius exercitationem, veritatis ipsum tempora
+      doloribus consequatur eveniet magnam, error vel. Incidunt harum odio et
+      officiis!Loremlorem Lorem ipsum dolor sit amet consectetur, adipisicing
+      elit. Voluptatum temporibus eos, iusto doloremque deleniti eius
+      exercitationem, veritatis ipsum tempora doloribus consequatur eveniet
+      magnam, error vel. Incidunt harum odio et officiis!Loremlorem Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptatum temporibus eos,
+      iusto doloremque deleniti eius exercitationem, veritatis ipsum tempora
+      doloribus consequatur eveniet magnam, error vel. Incidunt harum odio et
+      officiis!Loremlorem Lorem ipsum dolor sit amet consectetur, adipisicing
+      elit. Voluptatum temporibus eos, iusto doloremque deleniti eius
+      exercitationem, veritatis ipsum tempora doloribus consequatur eveniet
+      magnam, error vel. Incidunt harum odio et officiis!Loremlorem Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptatum temporibus eos,
+      iusto doloremque deleniti eius exercitationem, veritatis ipsum tempora
+      doloribus consequatur eveniet magnam, error vel. Incidunt harum odio et
+      officiis!Loremlorem Lorem ipsum dolor sit amet consectetur, adipisicing
+      elit. Voluptatum temporibus eos, iusto doloremque deleniti eius
+      exercitationem, veritatis ipsum tempora doloribus consequatur eveniet
+      magnam, error vel. Incidunt harum odio et officiis!Loremlorem Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptatum temporibus eos,
+      iusto doloremque deleniti eius exercitationem, veritatis ipsum tempora
+      doloribus consequatur eveniet magnam, error vel. Incidunt harum odio et
+      officiis!Loremlorem Lorem ipsum dolor sit amet consectetur, adipisicing
+      elit. Voluptatum temporibus eos, iusto doloremque deleniti eius
+      exercitationem, veritatis ipsum tempora doloribus consequatur eveniet
+      magnam, error vel. Incidunt harum odio et officiis!Loremlorem Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptatum temporibus eos,
+      iusto doloremque deleniti eius exercitationem, veritatis ipsum tempora
+      doloribus consequatur eveniet magnam, error vel. Incidunt harum odio et
+      officiis!Loremlorem Lorem ipsum dolor sit amet consectetur, adipisicing
+      elit. Voluptatum temporibus eos, iusto doloremque deleniti eius
+      exercitationem, veritatis ipsum tempora doloribus consequatur eveniet
+      magnam, error vel. Incidunt harum odio et officiis!Loremlorem Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptatum temporibus eos,
+      iusto doloremque deleniti eius exercitationem, veritatis ipsum tempora
+      doloribus consequatur eveniet magnam, error vel. Incidunt harum odio et
+      officiis!Loremlorem Lorem ipsum dolor sit amet consectetur, adipisicing
+      elit. Voluptatum temporibus eos, iusto doloremque deleniti eius
+      exercitationem, veritatis ipsum tempora doloribus consequatur eveniet
+      magnam, error vel. Incidunt harum odio et officiis!Loremlorem Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptatum temporibus eos,
+      iusto doloremque deleniti eius exercitationem, veritatis ipsum tempora
+      doloribus consequatur eveniet magnam, error vel. Incidunt harum odio et
+      officiis!Loremlorem Lorem ipsum dolor sit amet consectetur, adipisicing
+      elit. Voluptatum temporibus eos, iusto doloremque deleniti eius
+      exercitationem, veritatis ipsum tempora doloribus consequatur eveniet
+      magnam, error vel. Incidunt harum odio et officiis!Loremlorem Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptatum temporibus eos,
+      iusto doloremque deleniti eius exercitationem, veritatis ipsum tempora
+      doloribus consequatur eveniet magnam, error vel. Incidunt harum odio et
+      officiis!Loremlorem Lorem ipsum dolor sit amet consectetur, adipisicing
+      elit. Voluptatum temporibus eos, iusto doloremque deleniti eius
+      exercitationem, veritatis ipsum tempora doloribus consequatur eveniet
+      magnam, error vel. Incidunt harum odio et officiis!Loremlorem Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptatum temporibus eos,
+      iusto doloremque deleniti eius exercitationem, veritatis ipsum tempora
+      doloribus consequatur eveniet magnam, error vel. Incidunt harum odio et
+      officiis!Loremlorem Lorem ipsum dolor sit amet consectetur, adipisicing
+      elit. Voluptatum temporibus eos, iusto doloremque deleniti eius
+      exercitationem, veritatis ipsum tempora doloribus consequatur eveniet
+      magnam, error vel. Incidunt harum odio et officiis!Loremlorem Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptatum temporibus eos,
+      iusto doloremque deleniti eius exercitationem, veritatis ipsum tempora
+      doloribus consequatur eveniet magnam, error vel. Incidunt harum odio et
+      officiis!Loremlorem
+    </div>
+  );
 }
 
 export default Contact;
